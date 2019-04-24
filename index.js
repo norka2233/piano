7932b6a16e58c3fbcd03d32ed7ce7fade373e460
norka2233 <norka2233@gmail.com>
2019-04-24 21:45:31 +0300

    without working js

---
+++ index.js
@@ -0,0 +1,15 @@
+document.addEventListener('keydown', sound);
+document.addEventListener('keydown', remove);
+
+function sound(event){
+  audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  pressedKey = document.querySelector(`button[data-key="${event.keyCode}"]`);
+  pressedKey.className += " pressed_key";
+  audio.play();
+
+}
+
+function remove(event) {
+    pressedKey = document.querySelector(`button[data-key="${event.keyCode}"]`);
+    pressedKey.classList.remove('pressed_key');
+}
