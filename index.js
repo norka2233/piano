23de86a462791026f9239adba273c8b971a0c97a
norka2233 <norka2233@gmail.com>
2019-05-09 11:53:36 +0300

    last commit

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ document.addEventListener('keydown', sound);
 document.addEventListener('keyup', remove);
 
 function sound(event){
-  let audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-  let pressedKey = document.querySelector(`button[data-key="${event.keyCode}"]`);
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const pressedKey = document.querySelector(`button[data-key="${event.keyCode}"]`);
   pressedKey.className += " pressed_key";
   audio.play();
 
