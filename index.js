ddc3463ad688bfbc6777e1be15e42cf512913fdd
norka2233 <norka2233@gmail.com>
2019-05-09 11:44:11 +0300

    new const

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener('keydown', sound);
 document.addEventListener('keyup', remove);
 
 function sound(event){
-  let audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
   let pressedKey = document.querySelector(`button[data-key="${event.keyCode}"]`);
   pressedKey.className += " pressed_key";
   audio.play();
@@ -10,6 +10,6 @@ function sound(event){
 }
 
 function remove(event) {
-  let  pressedKey = document.querySelector(`button[data-key="${event.keyCode}"]`);
-  let pressedKey.classList.remove('pressed_key');
+   pressedKey = document.querySelector(`button[data-key="${event.keyCode}"]`);
+   pressedKey.classList.remove('pressed_key');
 }
